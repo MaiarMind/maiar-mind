d792268b65686f4bf134ec6d5cc61b9542a6b8f0
MaiarMind <aplicapymes@gmail.com>
2026-04-08 16:29:20 +0200

    Add docstring and import get_citricos_rules
    Added module docstring and imported citricos_agent_rules.

--- domains/citricos/__init__.py
+++ domains/citricos/__init__.py
@@ -1,1 +1,8 @@
+"""
+Dominio: Cítricos
+Especializado en detección de plagas, enfermedades, madurez y estrés hídrico en naranjos, mandarinos, limones, etc.
+"""
 
+from .citricos_agent_rules import get_citricos_rules
+
+__all__ = ["get_citricos_rules"]
